10c6ec9a008f51fefa081cbfc4622dfc8e8fa89b
Marwan <Marwan@doordeck.com>
2019-04-18 15:25:49 +0100

    adding sample app
    Signed-off-by: Marwan <Marwan@doordeck.com>

--- build/doordeck-sdk-swift.build/Release-iphoneos/doordeck-sdk-swift.build/DerivedSources/doordeck_sdk_swift_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char doordeck_sdk_swiftVersionString[];
- extern const double doordeck_sdk_swiftVersionNumber;
-
- const unsigned char doordeck_sdk_swiftVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:doordeck_sdk_swift  PROJECT:doordeck-sdk-swift-1" "\n";
- const double doordeck_sdk_swiftVersionNumber __attribute__ ((used)) = (double)1.;
