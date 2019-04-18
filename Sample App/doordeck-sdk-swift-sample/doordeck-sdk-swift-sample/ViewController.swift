6f93b4babb61afa4de780c4f7d145713e185dbf4
Marwan <Marwan@doordeck.com>
2019-04-18 15:15:45 +0100

    adding sample app
    Signed-off-by: Marwan <Marwan@doordeck.com>

---
+++ Sample App/doordeck-sdk-swift-sample/doordeck-sdk-swift-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  doordeck-sdk-swift-sample
+//
+//  Created by Marwan on 18/04/2019.
+//  Copyright © 2019 Doordeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
