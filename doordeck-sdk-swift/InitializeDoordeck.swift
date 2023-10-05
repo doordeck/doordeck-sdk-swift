5ddcee4c5e9ca5c3d9225f6c3aec6a6b0a387de5
Marwan <marwan@doordeck.com>
2023-10-05 15:27:48 +0200

    adding description

--- doordeck-sdk-swift/InitializeDoordeck.swift
+++ doordeck-sdk-swift/InitializeDoordeck.swift
@@ -113,6 +113,10 @@ public class Doordeck {
         darkModeActive(darkMode)
     }
     
+    /// This method can be used to puch tile UUID to the SDK to process
+    ///
+    /// - Parameters:
+    ///   - tileID: Tile UUID is UUID for a ile from a deeplink or QR or background NFC
     public func deeplink (_ tileID: String) {
         NotificationCenter.default.post(name: .deeplinkSDKCheck, object: tileID)
     }
