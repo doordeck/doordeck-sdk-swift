624d57cf927172306f8364ccf838027d5a3e1365
Marwan <marwan@doordeck.com>
2023-10-05 16:17:47 +0200

    spelling mistakes

--- doordeck-sdk-swift/InitializeDoordeck.swift
+++ doordeck-sdk-swift/InitializeDoordeck.swift
@@ -113,10 +113,10 @@ public class Doordeck {
         darkModeActive(darkMode)
     }
     
-    /// This method can be used to puch tile UUID to the SDK to process
+    /// This method can be used to push tile UUID to the SDK to process
     ///
     /// - Parameters:
-    ///   - tileID: Tile UUID is UUID for a ile from a deeplink or QR or background NFC
+    ///   - tileID: Tile UUID is UUID for a tile from a deeplink or QR or background NFC
     public func unlockTileID (_ tileID: String) {
         NotificationCenter.default.post(name: .deeplinkSDKCheck, object: tileID)
     }
